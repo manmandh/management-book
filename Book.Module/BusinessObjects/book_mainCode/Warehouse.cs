71e0269e99c72a5fc20ede02e975f857e7ea0d1c
manmandh <manmantran169@gmail.com>
2024-10-25 19:39:12 +0700

    first commit

---
+++ Book.Module/BusinessObjects/book_mainCode/Warehouse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace Book.Module.BusinessObjects.book_main
+{
+
+    public partial class Warehouse
+    {
+        public Warehouse(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
